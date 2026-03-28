4e7eb6d6ce861f0b43cb32a4143dd7760e411cea
sirasaki-konoha <rock-db@outlook.jp>
2026-03-28 22:27:02 +0900

    update test in reader.c: error flow update more modern

--- test/reader.c
+++ test/reader.c
@@ -7,20 +7,17 @@ int main() {
   KResult result;
   char* content;
 
-  result = k_reader_new(&reader, "Makefile.am");
-  if (K_FAILED(result)) {
-    k_result_print(result);
-    return -1;
-  }
+	K_CATCH(result, 
+			k_reader_new(&reader, "Makefile.am"),
+			return result.sys_errno);
 
-  result = k_reader_read_all(&reader, &content);
-  if (K_FAILED(result)) {
-    k_reader_free(&reader);
-    k_result_print(result);
-    return -1;
-  }
+	// allocate new memory buffer in k_reader_read_all
+	// requires free after use
+	K_WRAP(k_reader_read_all(&reader, &content), 
+			cleanup);
 
   printf("Makefile.am content: \n%s\n", content);
+	free(content);
 
   unsigned long total_len = k_reader_get_total_line(reader);
   printf("total lines = %lu\n", total_len);
@@ -29,6 +26,7 @@ int main() {
 
   result = k_reader_getline(&reader, total_len - 1, line_1, sizeof(char) * 255);
   if (K_FAILED(result)) {
+		free(line_1);
     k_reader_free(&reader);
     k_result_print(result);
     return -1;
@@ -37,6 +35,9 @@ int main() {
   printf("Makefile.am:1: %s\n", line_1);
 
   free(line_1);
-  k_reader_free(&reader);
-  free(content);
+	goto cleanup;
+
+cleanup:
+	k_reader_free(&reader);
+	return result.sys_errno;
 }
