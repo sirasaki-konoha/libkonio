1ebee00ffa8aa91ffefcd366e3a1e1f5c70b9b3c
shirasaki konoha <rock-db@outlook.jp>
2026-04-13 23:33:26 +0900

    improve to using kcore.h

--- src/konio.h
+++ src/konio.h
@@ -3,60 +3,13 @@
 
 #include <dirent.h>
 #include <stdio.h>
-
-#define K_FAILED(r) ((r).code != K_OK)
-#define K_SUCCEEDED(r) ((r).code == K_OK)
-
-#define K_WRAP(expr, label) \
-  do {                      \
-    KResult _r = (expr);    \
-    if (_r.code != K_OK) {  \
-      k_result_print(_r);   \
-      goto label;           \
-    }                       \
-  } while (0)
-
-#define K_WRAP_OR_GOTO(expr_call, on_error, label) \
-  do {                                             \
-    KResult _r = (expr_call);                      \
-    if (_r.code != K_OK) {                         \
-      on_error;                                    \
-      goto label;                                  \
-    }                                              \
-  } while (0)
-
-#define K_CATCH(res, expr_call, on_error) \
-  do {                                    \
-    (res) = (expr_call);                  \
-    if ((res).code != K_OK) {             \
-      on_error;                           \
-    }                                     \
-  } while (0)
+#include <kcore.h>
 
 
 typedef enum {
   APPEND,
   NEW,
 } WriteType;
-
-typedef enum {
-  K_OK = 0,
-  K_ERR_FILE_NOT_FOUND = 1,
-  K_ERR_PERMISSION,
-  K_ERR_IS_DIRECTORY,
-  K_ERR_NO_SPACE,
-  K_ERR_NO_MEMORY,
-  K_ERR_INVALID_ARG,
-  K_ERR_IO,
-  K_ERR_ALREADY_OPEN,
-  K_ERR_NOT_OPEN,
-  K_ERR_UNKNOWN,
-} K_RESULT;
-
-typedef struct {
-  K_RESULT code;
-  int sys_errno;
-} KResult;
 
 typedef struct {
   FILE* file;
@@ -76,10 +29,6 @@ typedef struct {
   DIR* dir;
 } Directory;
 
-/* Utility */
-KResult k_result_from_errno(int err);
-const char* k_result_str(KResult result);
-void k_result_print(KResult r);
 
 /* Writer */
 KResult k_writer_new(Writer* wt, const char* path, WriteType type);
